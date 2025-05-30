59cdf5ed314cd54835d17a44c25e512f54209c4d
Yuri Kevin <91747141+YuriKevin@users.noreply.github.com>
2025-05-30 17:37:02 -0300

    feat: Construtor da classe "Usuário".

--- src/main/java/com/usesoftware/biblioteca/model/Usuario.java
+++ src/main/java/com/usesoftware/biblioteca/model/Usuario.java
@@ -4,6 +4,11 @@ public abstract class Usuario {
 	private Long id;
 	private String nome;
 	
+	public Usuario(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+	
 	public Long getId() {
 		return id;
 	}
